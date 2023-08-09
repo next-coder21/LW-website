c84a92dba137496b1e2d1938e8dcc9ca64e3b1bb
Lijish-Wilson <106538925+next-coder21@users.noreply.github.com>
2023-08-09 12:40:21 +0530

    loader

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function validte(){
           return false;
         }
       }
+var loader = document.getElementById('preloader');
+const fadeOutEffect = setInterval(() => {
+  if (!preloader.style.opacity) {
+    preloader.style.opacity = 1;
+  }
+  if (preloader.style.opacity > 0) {
+    preloader.style.opacity -= 1;
+  } else {
+    clearInterval(fadeEffect);
+  }
+}, 5000);
